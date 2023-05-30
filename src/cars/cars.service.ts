c0e73e2b8f514402d8b1aa57579e71f4b0b4f41b
matias <matiasdlc95.mdlc@gmail.com>
2023-05-30 01:57:30 -0400

    inicio 02-nestjs

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CarInterface } from './interfaces/car.interface';
+
+@Injectable()
+export class CarsService {
+  private cars: CarInterface[] = [
+    {
+      id: uuid(),
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: uuid(),
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: uuid(),
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+    if (!car) {
+      throw new NotFoundException(`Car with id '${id}' not found`);
+    }
+    return car;
+  }
+}
---
+++ src/cars/interfaces/car.interface.ts
@@ -0,0 +1,5 @@
+export interface CarInterface {
+  id: string;
+  model: string;
+  brand: string;
+}
